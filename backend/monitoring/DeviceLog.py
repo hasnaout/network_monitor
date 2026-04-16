72ce00ab26b1d0d977b68a5181e0551a74f4d009
Hasna <hasn.outaleb@edu.umi.ac.ma>
2026-04-16 12:48:33 +0100

    new changes

--- backend/monitoring/DeviceLog.py
+++ backend/monitoring/DeviceLog.py
@@ -1,8 +1,6 @@
 class DeviceLog(models.Model):
     device = models.ForeignKey(Device, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
-
     cpu = models.FloatField(null=True)
     ram = models.FloatField(null=True)
-
     network_status = models.CharField(max_length=20) 
