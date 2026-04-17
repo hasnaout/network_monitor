402161ce9f565a0d2dbe8ad9576d40469334797a
Hasna <hasn.outaleb@edu.umi.ac.ma>
2026-04-17 08:25:35 +0100

    ajout fichier requirements

--- apps/rapport/models.py
+++ apps/rapport/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from apps.devices.models import Device
+from devices.models import Device
 
 class Report(models.Model):
     device = models.ForeignKey(Device, on_delete=models.CASCADE)
