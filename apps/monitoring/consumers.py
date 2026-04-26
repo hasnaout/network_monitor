68af14ee29a0b5955ef90393c301eacd7672282d
Hasna <hasnaoutaleb38@gmail.com>
2026-04-26 12:54:24 +0100

    reconstructions de consumer

--- apps/monitoring/consumers.py
+++ apps/monitoring/consumers.py
@@ -1,17 +1,13 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+
 class AlertConsumer(AsyncWebsocketConsumer):
 
-  
     async def connect(self):
         await self.channel_layer.group_add("alerts", self.channel_name)
         await self.accept()
 
-        await self.send(text_data=json.dumps({
-            "type": "system",
-            "message": "connected"
-        }))
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard("alerts", self.channel_name)
 
